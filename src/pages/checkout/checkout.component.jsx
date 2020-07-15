f5e149f04b953df2d184868b5252da0a05b7998a
layfredrc <frederic.lay@etu.parisdescartes.fr>
2020-07-16 01:31:42 +0200

    stripe integration + favicon update

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,6 +9,7 @@ import {
 import "./checkout.styles.scss";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
 	<div className='checkout-page'>
@@ -40,6 +41,13 @@ const CheckoutPage = ({ cartItems, total }) => (
 		<div className='total'>
 			<span>TOTAL: ${total}</span>
 		</div>
+		<div className='test-warning'>
+			*Please use the following test credit card for payment*
+			<br />
+			4242 4242 4242 4242 - Exp 01/20 -CVV: 123
+		</div>
+		<br />
+		<StripeCheckoutButton price={total} />
 	</div>
 );
 
